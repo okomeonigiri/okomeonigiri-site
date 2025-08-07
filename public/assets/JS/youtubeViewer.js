efb081d785974f32b512954938d74088195495a6
okome onigiri <kusashiod@yahoo.co.jp>
2025-08-07 23:55:22 +0900

    CSEむずいね

--- public/assets/JS/youtubeViewer.js
+++ public/assets/JS/youtubeViewer.js
@@ -8,3 +8,35 @@ function setup(){
     iframe.src="https://www.youtube-nocookie.com/embed/VIDEO_ID";
 }
 setup();
+
+function updateIframe(videoId) {
+    const iframe = document.querySelector("#youtube-iframe");
+    if (iframe) {
+        iframe.src = `https://www.youtube-nocookie.com/embed/${videoId}`;
+    }
+}
+
+window.addEventListener('load', function(){
+    const targetNode = document.querySelector('.gcse-searchresults');
+
+    const observer = new MutationObserver(function(mutationsList,observer){
+        const links = document.querySelectorAll('.gs-title');
+
+        links.forEach(function(link){
+            link.addEventListener('click',function(event){
+                console.log('Link clicked:', link);
+                event.preventDefault();
+
+                const url= link.getAttribute('data-ctorig')||link.href;
+
+                console.log( 'Clicked link:', url);
+            })
+        })
+    })
+
+    const config ={childList: true, subtree: true};
+    if(targetNode){
+        observer.observe(targetNode,config)
+    }
+});
+
